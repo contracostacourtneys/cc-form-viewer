525802d1850a4b7bcc1769f7749328a068ded00e
Michael Daly <Mdaly@contracosta.courts.ca.gov>
2018-05-22 12:26:02 -0700

    Added Textbox reducer

---
+++ client/assets/js/components/Textbox/reducer.js
@@ -0,0 +1,23 @@
+import { addToState, removeFromState } from 'Components/shared/addRemove';
+import validateComponent from 'Components/shared/validateComponent';
+
+const defaultState = {};
+
+
+module.exports = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'ADD_COMPONENT': {
+      if (!validateComponent(action.payload, 'Textbox')) {
+        return state;
+      }
+
+      return addToState(state, action.payload);
+    }
+
+    case 'REMOVE_COMPONENT': {
+      return removeFromState(state, action.payload, 'Textbox');
+    }
+  }
+
+  return state; 
+};
